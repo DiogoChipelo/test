eef3683554eecf0643daf6f7350fe322ddae73bb
DiogoTesteAccount <134712137+DiogoTesteAccount@users.noreply.github.com>
2024-02-28 17:01:32 +0000

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,12 +1,27 @@
-function soma(){ 
+function soma() { 
     let num1 = document.getElementById("num1").value;
     let num2 = document.getElementById("num2").value;
     let total = 0;
 
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    
-    total = num1 + num2;
+    if (num1 === "" && num2 === "") {
 
-    return document.getElementById("total").innerHTML = "A soma é " + total + " !";
+        alert("Faltam ambos os valores!");
+
+    } else if (num1 === "") {
+
+        alert("Falta o primeiro valor!");
+
+    } else if (num2 === "") {
+
+        alert("Falta o segundo valor!");
+
+    } else {
+
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+
+        total = num1 + num2;
+        
+        document.getElementById("total").innerHTML = "A soma é " + total + " !";
+    }
 }
